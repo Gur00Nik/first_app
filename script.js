2c745851699f9af871b93dbf7969b0c80b69fc22
nikita <ngurlov@gmail.com>
2024-04-06 11:06:03 +0300

    complete 2-3 exercise

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
+const firstEducation = {
+  nameFilms: prompt("Один из последних просмотренных фильмов", ""),
+  rating: +prompt("На сколько оцените его", ""),
+};
+const secondEducation = {
+  nameFilms: prompt("Один из последних просмотренных фильмов", ""),
+  rating: +prompt("На сколько оцените его", ""),
+};
+
 String(numberOfFilms) !== "NaN"
   ? alert(`Пользователь посмотрел: ${numberOfFilms} фильмов`)
   : alert(`Пользователь ввел не число`);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+    firstEducation,
+    secondEducation,
+  },
+  acrtors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
